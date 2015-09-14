1e0b5b857a014ab6779167394884e526992d5a88
Deyaa <dalmahal@indiana.edu>
2015-09-14 00:50:49 -0400

    converting annotizer module to point-free

--- src/Annotizer.hs
+++ src/Annotizer.hs
@@ -6,6 +6,10 @@
 
 module Annotizer where
 
+import Control.Arrow((***))
+import Control.Monad(join)
+import Data.List(foldl')
+
 import L1
   
 class Gradual p where
@@ -16,10 +20,10 @@ class Gradual p where
   -- 2^(number of all annotations) + number of complex types
   count   :: p -> Integer
   -- Counts the number of both all dynamic types and all types.
-  dynamic :: p -> Int -> Int -> (Int,Int)
+  dynamic :: p -> (Int,Int) -> (Int,Int)
   -- Counts the number of all static types.
   static  :: p -> Double
-  static e = let (dyns,alls) = dynamic e 0 0
+  static e = let (dyns,alls) = dynamic e (0,0)
              in if alls > 0
                 then fromIntegral (alls - dyns) / fromIntegral alls
                 else 0
@@ -80,63 +84,57 @@ instance Gradual e => Gradual (ExpF e) where
   count (Repeat _ e1 e2 e)    = count e1 * count e2 * count e
   count _                     = 1
 
-  dynamic (Op _ es) a1 a2           = foldP es a1 a2
-  dynamic (If e1 e2 e3) a1 a2       = foldP [e1,e2,e3] a1 a2
-  dynamic (App e1 es) a1 a2         = foldP (e1:es) a1 a2
-  dynamic (Lam _ e) a1 a2           = dynamic e a1 a2
-  dynamic (GRef e) a1 a2            = dynamic e a1 a2
-  dynamic (GDeRef e) a1 a2          = dynamic e a1 a2
-  dynamic (GAssign e1 e2) a1 a2     = foldP [e1,e2] a1 a2
-  dynamic (MRef e) a1 a2            = dynamic e a1 a2
-  dynamic (MDeRef e) a1 a2          = dynamic e a1 a2
-  dynamic (MAssign e1 e2) a1 a2     = foldP [e1,e2] a1 a2
-  dynamic (GVect e1 e2) a1 a2       = foldP [e1,e2] a1 a2
-  dynamic (GVectRef e1 e2) a1 a2    = foldP [e1,e2] a1 a2
-  dynamic (GVectSet e1 e2 e3) a1 a2 = foldP [e1,e2,e3] a1 a2
-  dynamic (MVect e1 e2) a1 a2       = foldP [e1,e2] a1 a2
-  dynamic (MVectRef e1 e2) a1 a2    = foldP [e1,e2] a1 a2
-  dynamic (MVectSet e1 e2 e3) a1 a2 = foldP [e1,e2,e3] a1 a2
-  dynamic (Let e1 e2) a1 a2         = let (a1',a2') = foldP e1 a1 a2
-                                      in dynamic e2 a1' a2'
-  dynamic (Letrec e1 e2) a1 a2      = let (a1',a2') = foldP e1 a1 a2
-                                      in dynamic e2 a1' a2'
-  dynamic (As e t) a1 a2            = let (a1',a2') = dynamic e a1 a2
-                                      in dynamic t a1' a2'
-  dynamic (Begin e' e) a1 a2        = let (a1',a2') = foldP e' a1 a2
-                                      in dynamic e a1' a2'
-  dynamic (Repeat _ e1 e2 e) a1 a2  = foldP [e1,e2,e] a1 a2
-  dynamic _ a1 a2                   = (a1,a2)
+  dynamic (Op _ es)           = foldP es
+  dynamic (If e1 e2 e3)       = foldP [e1,e2,e3]
+  dynamic (App e1 es)         = foldP (e1:es)
+  dynamic (Lam _ e)           = dynamic e
+  dynamic (GRef e)            = dynamic e
+  dynamic (GDeRef e)          = dynamic e
+  dynamic (GAssign e1 e2)     = foldP [e1,e2]
+  dynamic (MRef e)            = dynamic e
+  dynamic (MDeRef e)          = dynamic e
+  dynamic (MAssign e1 e2)     = foldP [e1,e2]
+  dynamic (GVect e1 e2)       = foldP [e1,e2]
+  dynamic (GVectRef e1 e2)    = foldP [e1,e2]
+  dynamic (GVectSet e1 e2 e3) = foldP [e1,e2,e3]
+  dynamic (MVect e1 e2)       = foldP [e1,e2]
+  dynamic (MVectRef e1 e2)    = foldP [e1,e2]
+  dynamic (MVectSet e1 e2 e3) = foldP [e1,e2,e3]
+  dynamic (Let e1 e2)         = dynamic e2 . foldP e1
+  dynamic (Letrec e1 e2)      = dynamic e2 . foldP e1
+  dynamic (As e t)            = dynamic t . dynamic e
+  dynamic (Begin e' e)        = dynamic e . foldP e'
+  dynamic (Repeat _ e1 e2 e)  = foldP [e1,e2,e]
+  dynamic _                   = id
 
-foldP :: Gradual a => [a] -> Int -> Int -> (Int,Int)
-foldP es dyns alls =
-  foldl (\(b1,b2) a -> dynamic a b1 b2) (dyns,alls) es
+foldP :: Gradual a => [a] -> (Int,Int) -> (Int,Int)
+foldP = flip $ foldl' $ flip dynamic
 
 instance Gradual e => Gradual (Bind e) where
-  lattice (x,t,e)       = (x,,) <$> lattice t <*> lattice e
-  count (_,t,e)         =  count t * count e
-  dynamic (_,t,e) a1 a2 = let (a1',a2') = dynamic t a1 a2
-                          in dynamic e a1' a2'
+  lattice (x,t,e) = (x,,) <$> lattice t <*> lattice e
+  count (_,t,e)   =  count t * count e
+  dynamic (_,t,e) = dynamic e . dynamic t
 
 instance Gradual Type where
-  lattice (GRefTy t)          = Dyn:(GRefTy <$> lattice t)
-  lattice (MRefTy t)          = Dyn:(MRefTy <$> lattice t)
-  lattice (GVectTy t)         = Dyn:(GVectTy <$> lattice t)
-  lattice (MVectTy t)         = Dyn:(MVectTy <$> lattice t)
-  lattice (FunTy t1 t2)       = Dyn:(FunTy <$> mapM lattice t1 <*> lattice t2)
-  lattice Dyn                 = [Dyn]
-  lattice t                   = [t,Dyn]
+  lattice (GRefTy t)    = Dyn:(GRefTy <$> lattice t)
+  lattice (MRefTy t)    = Dyn:(MRefTy <$> lattice t)
+  lattice (GVectTy t)   = Dyn:(GVectTy <$> lattice t)
+  lattice (MVectTy t)   = Dyn:(MVectTy <$> lattice t)
+  lattice (FunTy t1 t2) = Dyn:(FunTy <$> mapM lattice t1 <*> lattice t2)
+  lattice Dyn           = [Dyn]
+  lattice t             = [t,Dyn]
 
-  count (GRefTy t)            = count t + 1
-  count (MRefTy t)            = count t + 1
-  count (GVectTy t)           = count t + 1
-  count (MVectTy t)           = count t + 1
-  count (FunTy t1 t2)         = (product (map count t1) * count t2) + 1
-  count _                     = 2
+  count (GRefTy t)      = count t + 1
+  count (MRefTy t)      = count t + 1
+  count (GVectTy t)     = count t + 1
+  count (MVectTy t)     = count t + 1
+  count (FunTy t1 t2)   = (product (map count t1) * count t2) + 1
+  count _               = 2
 
-  dynamic Dyn a1 a2           = (a1+1,a2+1)
-  dynamic (GRefTy t) a1 a2    = dynamic t a1 a2
-  dynamic (MRefTy t) a1 a2    = dynamic t a1 a2
-  dynamic (GVectTy t) a1 a2   = dynamic t a1 a2
-  dynamic (MVectTy t) a1 a2   = dynamic t a1 a2
-  dynamic (FunTy t1 t2) a1 a2 = foldP (t2:t1) a1 a2
-  dynamic _ a1 a2             = (a1,a2+1)
+  dynamic Dyn           = join (***) (+1)
+  dynamic (GRefTy t)    = dynamic t
+  dynamic (MRefTy t)    = dynamic t
+  dynamic (GVectTy t)   = dynamic t
+  dynamic (MVectTy t)   = dynamic t
+  dynamic (FunTy t1 t2) = foldP (t2:t1)
+  dynamic _             = id *** (+1)
