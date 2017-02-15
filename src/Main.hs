0a2d35d89ef37d850febd779356f766f25fd9ccf
Deyaa <dalmahal@indiana.edu>
2017-02-15 01:24:35 -0500

    annotating every sampled source file by how much it is statically-typed

--- src/Main.hs
+++ src/Main.hs
@@ -21,7 +21,7 @@ writeLattice b dname dps =
   removePathForcibly dname >> createDirectoryIfMissing False dname >>
   foldM_ (\n p -> do
             h <- openFile (dname ++ show n ++ ".schml") WriteMode
-            hPrintf h ";; %.2f %% \n" (100*dynamic b p)
+            hPrintf h ";; %.2f %% \n" (100 * (fromIntegral $ static p :: Double) / fromIntegral b)
             hPutStrLn h (codeGen p)
             hClose h
             return (n+1)) (0 :: Int) dps
@@ -40,8 +40,9 @@ main = do
       putStrLn ("There are " ++ show a ++ " less precisely typed programs and " ++ show w ++ " type constructors")
       case tail args of
         [] -> writeLattice w dirPath $ lattice e
-        [ns] -> writeLattice w dirPath $ if a > 10000
-                                         then sampleUniformally e (read ns::Int)
-                                         else sampleUniformally' e (read ns::Int)
+        [ns] -> writeLattice w dirPath $
+          if a > 10000
+          then sampleUniformally e (read ns::Int)
+          else sampleUniformally' e (read ns::Int)
         [ns, nb] -> writeLattice w dirPath $ concat $ sampleFromBins e (read ns::Int) (read nb::Double)
         _ -> putStrLn "Invalid number of arguments"
