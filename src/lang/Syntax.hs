ee1babf881d4ebafe620fb153a6210c088435832
Deyaa <dalmahal@indiana.edu>
2017-03-06 01:00:22 -0500

    some lattice work

--- src/lang/Syntax.hs
+++ src/lang/Syntax.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE PatternSynonyms #-}
+{-# LANGUAGE BangPatterns #-}
 {-# OPTIONS_GHC -Wall -fno-warn-orphans #-}
 
 module Syntax(
@@ -9,9 +10,11 @@ module Syntax(
   Type(..),
   Ann(..),
   L,
-  foldAnn) where
+  foldAnn,
+  (⊑)) where
 
 import Text.Parsec.Pos (SourcePos)
+import Algebra.Lattice
 
 type Name         = String
 type Args         = [Name]
@@ -45,9 +48,55 @@ data Type =
   | TupleTy [Type]
   deriving (Eq,Show)
 
--- newtype Fix e = In {out::e (Fix e)}
+instance MeetSemiLattice Type where
+  Dyn /\ t                           = t
+  t /\ Dyn                           = t
+  CharTy /\ CharTy                   = CharTy
+  IntTy /\ IntTy                     = IntTy
+  FloatTy /\ FloatTy                 = FloatTy
+  BoolTy /\ BoolTy                   = BoolTy
+  UnitTy /\ UnitTy                   = UnitTy
+  (FunTy ts1 rt1) /\ (FunTy ts2 rt2) =
+    FunTy (zipWith (/\) ts1 ts2) $ (/\) rt1 rt2
+  (ArrTy ts1 rt1) /\ (ArrTy ts2 rt2) =
+    ArrTy (zipWith (/\) ts1 ts2) $ (/\) rt1 rt2
+  (RefTy t1) /\ (RefTy t2)           = RefTy $ (/\) t1 t2
+  (GRefTy t1) /\ (GRefTy t2)         = GRefTy $ (/\) t1 t2
+  (MRefTy t1) /\ (MRefTy t2)         = MRefTy $ (/\) t1 t2
+  (VectTy t1) /\ (VectTy t2)         = VectTy $ (/\) t1 t2
+  (GVectTy t1) /\ (GVectTy t2)       = GVectTy $ (/\) t1 t2
+  (MVectTy t1) /\ (MVectTy t2)       = MVectTy $ (/\) t1 t2
+  (TupleTy t1) /\ (TupleTy t2)       = TupleTy $ zipWith (/\) t1 t2
+  t1 /\ t2                             =
+    error ("/\\: undefined on " ++ show t1 ++ " and " ++ show t2)
 
--- data Ann a f x = Ann a (f x)
+instance JoinSemiLattice Type where
+  Dyn \/ _                           = Dyn
+  _ \/ Dyn                           = Dyn
+  CharTy \/ CharTy                   = CharTy
+  IntTy \/ IntTy                     = IntTy
+  FloatTy \/ FloatTy                 = FloatTy
+  BoolTy \/ BoolTy                   = BoolTy
+  UnitTy \/ UnitTy                   = UnitTy
+  (FunTy ts1 rt1) \/ (FunTy ts2 rt2) =
+    FunTy (zipWith (\/) ts1 ts2) $ (\/) rt1 rt2
+  (ArrTy ts1 rt1) \/ (ArrTy ts2 rt2) =
+    ArrTy (zipWith (\/) ts1 ts2) $ (\/) rt1 rt2
+  (RefTy t1) \/ (RefTy t2)           = RefTy $ (\/) t1 t2
+  (GRefTy t1) \/ (GRefTy t2)         = GRefTy $ (\/) t1 t2
+  (MRefTy t1) \/ (MRefTy t2)         = MRefTy $ (\/) t1 t2
+  (VectTy t1) \/ (VectTy t2)         = VectTy $ (\/) t1 t2
+  (GVectTy t1) \/ (GVectTy t2)       = GVectTy $ (\/) t1 t2
+  (MVectTy t1) \/ (MVectTy t2)       = MVectTy $ (\/) t1 t2
+  (TupleTy t1) \/ (TupleTy t2)       = TupleTy $ zipWith (\/) t1 t2
+  t1 \/ t2                           =
+    error ("\\/: undefined on " ++ show t1 ++ " and " ++ show t2)
+
+instance Lattice Type where
+
+(⊑) :: Type -> Type -> Bool
+(⊑) = joinLeq
+
 data Ann a e = Ann a (e (Ann a e))
 
 type L a = Ann SourcePos a
